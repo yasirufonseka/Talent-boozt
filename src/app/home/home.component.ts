cf7a5483dbab0c3ec5ecbf740f83d89389cca818
yasirufonseka <150530375+yasirufonseka@users.noreply.github.com>
2025-03-11 15:25:31 +0530

    with bootstrap

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  imports: [CardComponent]
 })
 export class HomeComponent {
 
